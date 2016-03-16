61c558357b0dce1609458cd83bf1c7e8d5e0dae1
Falk Pauser <falk.pauser@gmail.com>
2016-03-16 16:14:41 +0100

    Updated ember-cli & core-deps

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -47,6 +47,14 @@ module.exports = function(environment) {
     }
   };
 
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
   if (environment === 'test') {
     // Testem prefers this...
     ENV.baseURL = '/';
@@ -61,6 +69,10 @@ module.exports = function(environment) {
 
   if (environment === 'gh-pages') {
     ENV.baseURL = '/ember-table/';
+  }
+
+  if (environment === 'production') {
+
   }
 
   return ENV;
